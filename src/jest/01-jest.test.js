63e045909494ea9b60381078fc7c883f6c53624e
jenien <riska.jeen@gmail.com>
2024-06-24 03:58:12 +0700

    fix

--- src/jest/01-jest.test.js
+++ src/jest/01-jest.test.js
@@ -1,17 +1,30 @@
 /* eslint-disable no-undef */
-// test("testing sederhana", () => {
-//   expect(true).toBe(true);
-// });
-
-test("menjumlahkan nilai", () => {
-  expect(1 + 1).toBe(2);
-  expect(2 * 3).toBe(6);
+test("operasi aritmatika dasar", () => {
+  expect(3 + 2).toBe(5);
+  expect(7 - 3).toBe(4);
+  expect(2 * 4).toBe(8);
 });
 
-test("fungsi menjumlahkan nilai 2", () => {
-  function sum(a, b) {
+test("fungsi penjumlahan", () => {
+  function add(a, b) {
     return a + b;
   }
-  expect(sum(1, 2)).toBe(3);
-  expect(sum(-1, 1)).toBe(0);
+  expect(add(2, 3)).toBe(5);
+  expect(add(-3, 3)).toBe(0);
+});
+
+test("fungsi pengurangan", () => {
+  function subtract(a, b) {
+    return a - b;
+  }
+  expect(subtract(5, 2)).toBe(3);
+  expect(subtract(7, 3)).toBe(4);
+});
+
+test("fungsi perkalian", () => {
+  function multiply(a, b) {
+    return a * b;
+  }
+  expect(multiply(3, 3)).toBe(9);
+  expect(multiply(5, -2)).toBe(-10);
 });
