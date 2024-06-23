51fb9f804daa2b5f5e0fe4410f72da179259bfd4
jenien <riska.jeen@gmail.com>
2024-06-24 02:58:08 +0700

    first commit

---
+++ src/jest/01-jest.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+// test("testing sederhana", () => {
+//   expect(true).toBe(true);
+// });
+
+test("menjumlahkan nilai", () => {
+  expect(1 + 1).toBe(2);
+  expect(2 * 3).toBe(6);
+});
+
+test("fungsi menjumlahkan nilai 2", () => {
+  function sum(a, b) {
+    return a + b;
+  }
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(-1, 1)).toBe(0);
+});
